2a912c4216129536e061c707343fbed716fcd8bc
Yeom MG <threebr123@naver.com>
2023-04-15 16:35:08 +0900

    베이스 프로젝트 통합본 v0.06
    1. 미니게임데이터매니저 추가
    위치 : Script/Minigame
    2. 백엔드 서버 통합 및 미니게임별 랭킹/테이블 구분

---
+++ 데모용 통합본/BaseProject/Assets/Scripts/MiniGame/BasketballGameScene/GameOver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] GameObject gameOverText;
+    [SerializeField] float maxTime =5f;
+    float timeLeft;
+    Image timerBar;
+
+    void Start()
+    {
+        gameOverText.SetActive(false);
+        timerBar = GetComponent<Image>();
+        timeLeft = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maxTime;
+        }
+        else
+        {
+            gameOverText.SetActive(true);
+           // Time.timeScale = 0;
+        }
+    }
+}
