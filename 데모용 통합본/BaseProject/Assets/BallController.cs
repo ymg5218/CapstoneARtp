fb1bb2fc7ec47c0867228af5ddd209ec5a49e964
Park Younghoon <130918433+parkyounghoon1@users.noreply.github.com>
2023-04-17 00:30:11 +0900

    a

---
+++ 데모용 통합본/BaseProject/Assets/BallController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    public float resetTime = 3.0f;
+    public Text result;
+    public int goal_point = 0;
+    Rigidbody rb;
+    bool isReady = true;
+    Vector2 startPos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+
+        result.text = "";
+    }
+
+    void Update()
+    {
+        if (!isReady)
+        {
+            return;
+        }
+
+       SetBallPosition(Camera.main.transform);
+
+        if (Input.touchCount > 0 && isReady)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                startPos = touch.position;
+            }
+
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                float dragDistance = touch.position.y - startPos.y;
+
+                Vector3 throwAngle = (Camera.main.transform.forward + Camera.main.transform.up).normalized;
+
+                rb.isKinematic = false;
+                isReady = false;
+
+                //직선 발사
+                //rb.AddForce(throwAngle * dragDistance * 0.005f, ForceMode.VelocityChange);
+
+                //포물선 발사
+                rb.AddForce((throwAngle * dragDistance / 10.0f) + Vector3.up * dragDistance);
+
+                Invoke("ResetBall", resetTime);
+
+            }
+        }
+
+    }
+
+    void SetBallPosition(Transform anchor)
+    {
+        Vector3 offset = anchor.forward * 0.5f + anchor.up * -0.2f;
+
+        transform.position = anchor.position + offset;
+    }
+
+    void ResetBall()
+    {
+        rb.isKinematic = true;
+        rb.velocity = Vector3.zero;
+
+        isReady = true;
+
+        gameObject.SetActive(true);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isReady)
+        {
+            return;
+        }
+        if (collision.gameObject.tag == "Hoop")
+        {
+            goal_point += 1;
+            result.text = goal_point.ToString();
+            gameObject.SetActive(false);
+        }
+    }
+}
--- 데모용 통합본/BaseProject/Assets/Scripts/MiniGame/BasketBallGameScene/Script/PlaceHoop.cs
+++ 데모용 통합본/BaseProject/Assets/Scripts/MiniGame/BasketBallGameScene/Script/PlaceHoop.cs
@@ -8,45 +8,35 @@ using UnityEngine.XR.ARSubsystems;
 public class PlaceHoop : MonoBehaviour
 {
     [SerializeField]
-    [Tooltip("Instantiates this hoop prefab on a plane at the touch location.")]
+    [Tooltip("터치한 위치에 후프가 생길 것입니다.")]
     GameObject m_HoopPrefab;
-
+    /// 터치하면 후프가 생김
     public GameObject placedHoop
     {
         get { return m_HoopPrefab; }
         set { m_HoopPrefab = value; }
     }
 
-
     public GameObject spawnedHoop { get; private set; }
 
     [SerializeField]
-    [Tooltip("Instantiates this ball prefab in front of the AR Camera.")]
+    [Tooltip("카메라 앞에 공이 생깁니다.")]
     GameObject m_BallPrefab;
 
-    /// <summary>
-    /// The prefab to instantiate on touch.
-    /// </summary>
     public GameObject placedBall
     {
         get { return m_BallPrefab; }
         set { m_BallPrefab = value; }
     }
 
-    /// <summary>
-    /// The object instantiated as a result of a successful raycast intersection with a plane.
-    /// </summary>
 
     public GameObject spawnedBall { get; private set; }
 
 
-    /// <summary>
-    /// Invoked whenever an object is placed in on a plane.
-    /// </summary>
     public static event Action onPlacedObject;
 
     private bool isPlaced = false;
-
+    
     ARRaycastManager m_RaycastManager;
 
     static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
@@ -75,8 +65,8 @@ public class PlaceHoop : MonoBehaviour
                     spawnedHoop.transform.parent = transform.parent;
 
                     isPlaced = true;
-                    spawnedBall = Instantiate(m_BallPrefab);
-                    spawnedBall.transform.parent = m_RaycastManager.transform.Find("AR Camera").gameObject.transform;
+               //     spawnedBall = Instantiate(m_BallPrefab);
+               //     spawnedBall.transform.parent = m_RaycastManager.transform.Find("AR Camera").gameObject.transform;
                     if (onPlacedObject != null)
                     {
                         onPlacedObject();
@@ -86,4 +76,3 @@ public class PlaceHoop : MonoBehaviour
         }
     }
 }
-
